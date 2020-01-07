43eb91d6edda8560d80ea8e522460bd0cc83b165
Adam Greene <agreene@cvent.com>
2020-01-07 10:54:01 -0400

    Added Grpc example Added RazorPages example

---
+++ MonctonUG.Grpc.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using MonctonUG.Grpc.Server;
+
+namespace MonctonUG.Grpc.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // This line is not required on Windows / Linux (only Mac)
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var channel = GrpcChannel.ForAddress("http://localhost:6000");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Adam" });
+            Console.WriteLine($"Greeting {reply.Message}");
+        }
+    }
+}
