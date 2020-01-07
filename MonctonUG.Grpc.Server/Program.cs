e424c47c2ca9f44004152a9e580fb668c2cd08e8
Adam Greene <agreene@cvent.com>
2020-01-07 11:10:23 -0400

    Added missing files

---
+++ MonctonUG.Grpc.Server/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace MonctonUG.Grpc.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    // This is only required on Mac (it doesn't support HTTP/2 with TLS
+                    webBuilder.ConfigureKestrel(serverOptions => {
+                        serverOptions.ListenAnyIP(6000, listenOptions => {
+                            listenOptions.Protocols = HttpProtocols.Http2;
+                        });
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
